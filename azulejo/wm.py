9b5619e4884ee82f3eaa8e38e775d2ce2cab4578
Pedro <p@p-Aspire-R3600.(none)>
2012-02-16 01:29:33 +0100

    added window manager interface

---
+++ azulejo/wm.py
@@ -0,0 +1,40 @@
+class Desktop:
+
+    def __init__(self):
+        return null
+    
+    def getallscreens(self):
+        return null
+
+
+class screen:
+    
+    def __init__(self):
+        return null
+
+    def getsize(self):
+        return null
+
+    def getallwindows(self):
+        return null
+
+    def getnumber(self):
+        return null
+
+
+class window:
+    def __init__(self):
+        return null
+
+    def getgeometry(self):
+        return null
+
+    def setgeometry(self):
+        return null
+
+    def getscreen(self):
+        return null
+
+    def movetoscreen(self):
+        return null
+
